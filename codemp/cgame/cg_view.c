d06e074e1ff9d74f5c20aef481971185e14dc878
Huben <hbazgier@gmail.com>
2026-09-10 15:38:07 +0200

    force first person when entering binoculars with saber or melee

--- codemp/cgame/cg_view.c
+++ codemp/cgame/cg_view.c
@@ -3058,7 +3058,8 @@ void CG_DrawActiveFrame( int serverTime, stereoFrame_t stereoView, qboolean demo
 			cg.predictedPlayerState.forceHandExtend == HANDEXTEND_KNOCKDOWN || cg.predictedPlayerState.fallingToDeath ||
 			cg.predictedPlayerState.m_iVehicleNum || PM_InKnockDown(&cg.predictedPlayerState))
 		{
-			if (cg.predictedPlayerState.zoomMode && (cg.predictedPlayerState.weapon == WP_SABER || cg.predictedPlayerState.weapon == WP_MELEE))
+			if (cg.predictedPlayerState.zoomMode &&
+				(cg.predictedPlayerState.weapon == WP_SABER || cg.predictedPlayerState.weapon == WP_MELEE))
 			{ //force first person when entering binoculars with saber or melee
 				cg.renderingThirdPerson = qfalse;
 			}
