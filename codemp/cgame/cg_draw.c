53b5f48bd9ca486eed7b6a98d2fd4130f0111f99
Huben <hbazgier@gmail.com>
2026-09-12 13:06:36 +0200

    hide health dispenser (third grey bacta) icon if gamemode is FFA

--- codemp/cgame/cg_draw.c
+++ codemp/cgame/cg_draw.c
@@ -6030,8 +6030,8 @@ static void CG_DrawInventory(int y)
 
 	for (i = 0; i < HI_NUM_HOLDABLE; i++)
 	{
-		// Hide icons for jetpack and health dispenser (third grey bacta icon) from side inventory
-		if (i && i != HI_JETPACK && i != HI_HEALTHDISP && cg.snap->ps.stats[STAT_HOLDABLE_ITEMS] & (1 << i))
+		// Hide jetpack icon always; hide health dispenser icon only in FFA
+		if (i && i != HI_JETPACK && (i != HI_HEALTHDISP || cgs.gametype != GT_FFA) && cg.snap->ps.stats[STAT_HOLDABLE_ITEMS] & (1 << i))
 		{
 			CG_DrawPic(xAlign, y, ico_size*cgs.widthRatioCoef, ico_size, cgs.media.invenIcons[i]);
 			y += ico_size;
